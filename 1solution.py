5f184126de8c496405d7b92d7dce30e7f23613fb
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-12-28 05:20:54 +0200

    improved solution

--- 1solution.py
+++ 1solution.py
@@ -5,12 +5,11 @@ number = random.randint(-10000, 10000)
 if number < 0:
     x = number * -1
     y = x % 10
-    print(y)
+
 else:
     y = number % 10
-    print(y)
 
-print(f'Last number of {number} is {y}')
+print(f'Last number of {number} is {y} ', end="")
 if y > 5 :
     print("and is greater than 5")
 elif y == 0 :
