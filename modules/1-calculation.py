a3ab1582e8b09f040f6d44bcc8ec9efd00985434
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-12-31 00:28:32 +0200

    argv

--- modules/1-calculation.py
+++ modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import calculator_1 as cal
+import calculator_1
 
 a = 10
 b = 5
 
-print(f"{a} + {b} = {cal.add(a, b)}")
-print(f"{a} - {b} = {cal.sub(a, b)}")
-print(f"{a} * {b} = {cal.mul(a, b)}")
-print(f"{a} / {b} = {cal.div(a, b)}")
+print(f"{a} + {b} = {calculator_1.add(a, b)}")
+print(f"{a} - {b} = {calculator_1.sub(a, b)}")
+print(f"{a} * {b} = {calculator_1.mul(a, b)}")
+print(f"{a} / {b} = {calculator_1.div(a, b)}")
---
+++ modules/2-args.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import sys
+
+for i in range(1, len(sys.argv), 1):
+    print(f"{i}: {sys.argv[i]}")
