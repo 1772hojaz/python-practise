1b85c72819c5f18dad4bbc46d9df20dbf5003fe0
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-03-08 00:41:48 +0200

    I, UPDATED #stacks/linked_list_stack.py

--- stacks/linked_list_stack.py
+++ stacks/linked_list_stack.py
@@ -5,9 +5,6 @@ class Node:
         self.data = data
         self.next = None
 
-    def pop_l(self):
-        pass
-
 
 n1 = Node(1)
 n2 = Node(2)
@@ -17,7 +14,27 @@ n3 = Node(3)
 n1.next = n2
 n2.next = n3
 
+head = n1
+current = head
+while current :
+    print(current.data)
+    current = current.next
+    
 
-while n1 :
-    print(n1.data)
-    n1 = n1.next
+print("The new list")
+
+tmp = head
+
+while tmp:
+    if tmp.next is None:
+        tmp.next = Node(4)
+        break
+    tmp = tmp.next
+
+check = head
+
+while check:
+    print(check.data)
+    check = check.next
+
+
