38c0fae5ed141b6c3190cd7e93d43b72407bea63
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-03-09 00:57:32 +0200

    I, UPDATED #stacks/linked_list.py

--- stacks/linked_list.py
+++ stacks/linked_list.py
@@ -16,6 +16,16 @@ class Node:
         while head:
             print(head.data)
             head = head.next
+
+    def add(self, element, position):
+        head = self
+        new_Node = Node(element)
+        if position == 1:
+            new_node.next = head
+            return new_node
+
+        elif:
+        
 n1 = Node(1)
 n2 = Node(2)
 n3 = Node(3)
