7cb1ef23551453d4b02a9576aca44d01eaa048db
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-06-04 18:34:48 +0200

    I, UPDATED #node.py

--- node.py
+++ node.py
@@ -1,40 +1,18 @@
-#!/usr/bin/python3
+#!/bin/python3
 
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-   
-    def get_val(self):
-        head = self
-        val = []
-        while head:
-            val.append(head.data)
-            head = head.next
-        return val
-   
-    def reverse(self, a):
-        return a[::-1]
-   
-    def tosting(self, a):
-        res = ''.join(map(str,a))
-        return res
-   
-    def toint(self, a):
-        return int(a)
 
 
+n1 = Node(12)
+n2 = Node(13)
+n3 = Node(14)
 
+n1.next = n2
+n2.next = n3
 
-node1 = Node(1)
-node2 = Node(2)
-node3 = Node(3)
-
-
-node1.next = node2
-node2.next = node3
-
-
-# Using Nodes
-print((node1.toint(node1.tosting(node1.reverse(node1.get_val())))))
-
+while n1:
+    print(f"the value is {n1.data}")
+    n1 = n1.next
