c41d0db24b63b14112fed154441102bae9c6563e
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-06-04 18:55:46 +0200

    I, UPDATED #node.py

--- node.py
+++ node.py
@@ -5,14 +5,3 @@ class Node:
         self.data = data
         self.next = None
 
-
-n1 = Node(12)
-n2 = Node(13)
-n3 = Node(14)
-
-n1.next = n2
-n2.next = n3
-
-while n1:
-    print(f"the value is {n1.data}")
-    n1 = n1.next
