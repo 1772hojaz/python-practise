373e8a8511ed52c7cea6c9e71a62fe17cf471301
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-08-26 18:46:55 +0200

    adding two numbers stored as n-bit binary array

---
+++ 3rded/adding_binary.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+# adding bit-binary integers stored in two arrays
+
+def add(a, b):
+        result = []
+        carry = 0
+        i = len(a)
+        while i >= 0:
+            x = a[i] + b[i] + carry
+            result.append(x%2)
+            carry = x//2
+            i-=1
+        return result
