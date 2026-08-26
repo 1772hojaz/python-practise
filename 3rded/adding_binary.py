7551855ca7ea4d965e22ac1292c28163eb2fe9eb
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-08-26 22:02:13 +0200

    I ADDED #3rded/.test.py.swp, UPDATED #3rded/adding_binary.py,#3rded/test.py

--- 3rded/adding_binary.py
+++ 3rded/adding_binary.py
@@ -5,10 +5,11 @@
 def add(a, b):
         result = []
         carry = 0
-        i = len(a)
+        i = len(a)-1
         while i >= 0:
             x = a[i] + b[i] + carry
             result.append(x%2)
             carry = x//2
             i-=1
+            print(carry)
         return result
--- 3rded/test.py
+++ 3rded/test.py
@@ -1,10 +1,18 @@
 #!/bin/python3
 #from insertion import insertsort as ins
 #from nonincreasing import nonsort as non
-from linear import linear as l
-a  = [31,41,59,26,41,58]
+#from linear import linear as l
+#a  = [31,41,59,26,41,58]
 #print(ins(a))
 #print(non(a))
 
-v = 59
-print(l(a, v))
+#v = 59
+#print(l(a, v))
+
+from adding_binary import add
+
+a = [0,1,0]
+
+b = [1,1,0]
+
+print(add(a,b))
