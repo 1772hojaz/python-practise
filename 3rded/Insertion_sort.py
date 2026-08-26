42f4b6eca38fc70d6c22cdbc6f0ab41e4c4f89b1
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-08-26 10:43:15 +0200

    I, UPDATED #3rded/Insertion_sort.py

--- 3rded/Insertion_sort.py
+++ 3rded/Insertion_sort.py
@@ -3,3 +3,15 @@
 a = [5,2,4,6,1,3]
 
 #sort this using insertion sorting
+
+def insertsort(arr):
+    for j in range(1, len(arr)):
+        key = arr[j]
+        # the key holds the potato value in my hand
+        i = j - 1
+        while i >= 0 and arr[i] > key:
+            arr[i+1] = arr[i]
+            i -= 1
+            arr[i+1] = key
+    return arr
+print(insertsort(a))
